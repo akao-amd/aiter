bee9c8ded4c5083ab8904156fbc0709407d704ff
Alan Kao <akao@amd.com>
2026-06-30 20:29:30 +0800

    [AAI sprint] skip the skinny kernel for gfx1250

--- aiter/tuned_gemm.py
+++ aiter/tuned_gemm.py
@@ -274,7 +274,11 @@ def get_GEMM_A16W16_config(
                 assert (
                     False
                 ), f"no solution for {M=} {N=} {K=} {dtype=} {bias=}, {scaleAB=}, {bpreshuffle=}"
-        elif is_skinny_default_shape(M, N, K, dtype, cu_num):
+        elif is_skinny_default_shape(M, N, K, dtype, cu_num) and gfx in {
+            "gfx90a",
+            "gfx942",
+            "gfx950",
+        }:
             # soltype, solution_idx = 3, 2
             default_config["libtype"] = "skinny"
             default_config["solidx"] = 2
